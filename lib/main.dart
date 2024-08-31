2f05717e97b81b5aee91a793e7f599290232f8f5
Adegungu1 <enocholubayo15@gmail.com>
2024-08-31 15:46:14 +0100

    first

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      );
+  }
+}
+
+class HomePage extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
